0e84c535fd5768fc2455842ff92ae8f23070a9bd
Alfian Hidayat <alfian08.ah@gmail.com>
2016-12-09 13:47:06 +0700

    Initial commit

---
+++ app/src/main/java/com/alfianh/project0/MainActivity.java
@@ -0,0 +1,24 @@
+package com.alfianh.project0;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnCapstone:
+                Toast.makeText(this, "This button will launch my capstone project", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
